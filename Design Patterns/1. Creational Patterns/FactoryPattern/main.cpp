fbcb2cb4a1b94be7ffd7653504484d7b35f25d61
Aman Singhal <singhalaman198@gmail.com>
2024-10-31 07:57:38 +0530

    calling different functions for creating cars

--- Design Patterns/1. Creational Patterns/FactoryPattern/main.cpp
+++ Design Patterns/1. Creational Patterns/FactoryPattern/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include "ScorpioFactory.hpp"
 #include "Scorpio.hpp"
+#include "ScorpioN.hpp"
+#include "ScorpioClassic.hpp"
+#include <vector>
 using namespace std;
 
 int main()
 {
     // first i created the factory object
-    ScorpioFactory *factory = new ScorpioFactory();
+    // ScorpioFactory *factory = new ScorpioFactory();
 
     // using that factory object, I created the object of specific car i want.
-    Scorpio *Car1 = factory->createScorpioFactory('N');
+    // Scorpio *Car1 = factory->createScorpioFactory('N');
 
     // call the functions of that car object.
-    Car1->driveCar();
+    // Car1->driveCar();
+
+    vector<Scorpio *> list;
+    Scorpio *car1 = new ScorpioN();
+    Scorpio *car2 = new ScorpioClassic();
+    list.push_back(car1);
+    list.push_back(car2);
+
+    for (int i = 0; i < list.size(); i++)
+    {
+        list[i]->driveCar();
+    }
+
     return 0;
 }
